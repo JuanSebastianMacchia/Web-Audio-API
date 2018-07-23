60e2f61b11454252b23ac857357475d00145cee6
sebamacchia <jsmacchia@yahoo.es>
2018-07-23 15:49:08 -0300

    c

---
+++ audio.js
@@ -0,0 +1,10 @@
+
+// OSCILATOR EXAMPLE
+
+var audioCtx = new (window.AudioContext || window.webkitAudioContext);
+
+var sine = audioCtx.createOscillator();
+
+sine.start();
+sine.connect(audioCtx.destination);
+
